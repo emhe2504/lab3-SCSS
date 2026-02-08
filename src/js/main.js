f29bbefc8e6034d8306590f3e76bec2a955727c9
Emma Heikkinen <emhe2504@student.miun.se>
2026-02-08 13:42:32 +0100

    Färgändringar, ny bild och styling

--- src/js/main.js
+++ src/js/main.js
@@ -10,9 +10,11 @@ function getAnswer() {
     const button = document.getElementById("forAnswer");
     const answer = document.getElementById("answer");
     const bubble = document.getElementById("bubble3");
+    const letter = document.getElementById("letter");
 
     if(bubble)        
     button.addEventListener("click", () => {
         answer.classList.remove("is_hidden");
+        letter.classList.remove("is_hidden");
     })
 }
