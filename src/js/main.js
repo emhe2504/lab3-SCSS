ba8c4bfcbc189a905e6df8ca0f7426c1dab14c66
Emma Heikkinen <emhe2504@student.miun.se>
2026-02-08 12:29:50 +0100

    Js på knapp

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 "use strict"
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    getAnswer();
+}
+
+function getAnswer() {
+    const button = document.getElementById("forAnswer");
+    const answer = document.getElementById("answer");
+    const bubble = document.getElementById("bubble3");
+
+    if(bubble)        
+    button.addEventListener("click", () => {
+        answer.classList.remove("is_hidden");
+    })
+}
